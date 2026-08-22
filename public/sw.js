bca0a093bed9559833960061394e447bea74766b
newtruesite <truelegacyworld@proton.me>
2026-08-22 14:46:21 -0500

    chore: bump PWA cache to v3 to force live reload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'true-legacy-app-v2'
+const CACHE = 'true-legacy-app-v3'
 const STATIC_ASSETS = ['/offline.html', '/manifest.webmanifest', '/icons/icon-192.png', '/icons/icon-512.png']
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(STATIC_ASSETS))))
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())))
